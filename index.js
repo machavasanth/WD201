a5e4ade83907e0a4bb036a61b0102fdd55053fee
machavasanth <113611987+machavasanth@users.noreply.github.com>
2022-10-20 18:38:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
-const hello = () => {
-  console.log("Hello Node.js!");
-};
+const http = require("http");
+const fs = require("fs");
+var port = process.argv.slice(2)
+port = parseInt(String(port).slice(7))
+console.log(port);
 
-hello();
+let homeData = "";
+let projectData = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeData = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectData = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+http
+    .createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/project":
+                response.write(projectData);
+                response.end();
+                break;
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            default:
+                response.write(homeData);
+                response.end();
+                break;
+        }
+    })
+    .listen(port);
